f81a88c045eedfb253a1755161a2696172e3e9be
sparib <sparibtheonly@gmail.com>
2021-01-31 15:41:55 -0500

    BlueLeftDrop ver 1.0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mains/BlueLeftDrop.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.mains;
+
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.teamcode.libraries.Alexi;
+import org.firstinspires.ftc.teamcode.libraries.AutonomousFunctions;
+import org.firstinspires.ftc.teamcode.libraries.CameraAuto;
+import org.firstinspires.ftc.teamcode.libraries.CameraType;
+import org.firstinspires.ftc.teamcode.libraries.EasyOpenCVImportable;
+import org.firstinspires.ftc.teamcode.libraries.TeleAuto;
+
+@Autonomous(name="Blue Left Drop", group="Blue")
+public class BlueLeftDrop extends LinearOpMode implements TeleAuto {
+    Alexi robot = new Alexi();
+    CameraAuto drive = new CameraAuto();
+    AutonomousFunctions auto = new AutonomousFunctions(robot, this);
+    EasyOpenCVImportable openCV = new EasyOpenCVImportable();
+    ElapsedTime et = new ElapsedTime();
+
+    public void runOpMode() {
+        robot.init(hardwareMap);
+
+        drive.setUp(robot.motors, robot.angles, robot.camera, robot.imu, AxesReference.EXTRINSIC, telemetry);
+        drive.setPose(new Pose2d(-56, 17, new Rotation2d()));
+
+        openCV.init(CameraType.WEBCAM, hardwareMap);
+        openCV.setBox(80, 110, 70, 50);
+        openCV.setThresholds(135, 148);
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            robot.camera.start();
+            openCV.startDetection();
+            et.reset();
+            while (et.seconds() < 1) { idle(); }
+            if (openCV.getDetection().equals(EasyOpenCVImportable.RingNumber.NONE)) {
+                openCV.stopDetection();
+                drive.goTo(10, 50, 0, this);
+                auto.armDownAndDrop();
+                drive.goToPosition(0, 50, this);
+            }
+        }
+    }
+}
