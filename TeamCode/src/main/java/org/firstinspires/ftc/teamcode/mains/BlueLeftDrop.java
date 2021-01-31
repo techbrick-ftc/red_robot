f4650daa7a04ebd33a1b9a135854ab08632e2c13
sparib <sparibtheonly@gmail.com>
2021-01-31 15:59:05 -0500

    BlueLeftDrop ver 1.0.3 Changed drive position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mains/BlueLeftDrop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mains/BlueLeftDrop.java
@@ -47,7 +47,7 @@ public class BlueLeftDrop extends LinearOpMode implements TeleAuto {
                 openCV.stopDetection();
                 drive.goTo(10, 45, 0, this);
                 auto.armDownAndDrop();
-                drive.goToPosition(12, 45, this);
+                drive.goToPosition(6, 0, this);
             }
         }
     }
